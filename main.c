c556f6526057562c453814b79fce82732f606bf6
Embedotronics <34275091+Embedotronics@users.noreply.github.com>
2020-04-27 16:59:24 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,31 @@
+/*
+ * push_button.c
+ *
+ * Created: 24-04-2020 14:56:02
+ * Author : Pawan
+ */ 
+
+# define F_CPU 8000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void)
+{
+    /* Replace with your application code */
+	DDRA=0x00;
+	DDRD=0xff;
+    while (1) 
+    {
+		if(PINA==0x01)
+		{
+			_delay_ms(50);
+			PORTD=1<<5;
+		}
+		else if(PINA==0x00){
+			_delay_ms(50);
+			PORTD=0<<5;
+		}
+		
+    }
+}
+
